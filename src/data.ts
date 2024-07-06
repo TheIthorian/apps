74a2d6088c2fe1e4257273eb67bfc236998f7b3d
TheIthorian <ben.shortland1020@gmail.com>
2024-07-06 21:50:21 +0100

    =chore: bump url

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 export const data = {
-    foundry: 'https://foundryvtt.com/',
+    foundry: 'https://demo.foundryvtt.com/join',
     'theatre-schedule': 'https://github.com/TheIthorian/theatre-schedule',
     'energy-dashboard': 'https://github.com/TheIthorian/energy-usage-dashboard',
 };
