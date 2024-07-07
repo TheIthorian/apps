035c59c01ab19dbf106892a65c6e2d3bf5cf118c
TheIthorian <65973510+TheIthorian@users.noreply.github.com>
2024-07-07 13:46:55 +0100

    =chore: bump url

--- src/data.ts
+++ src/data.ts
@@ -1,5 +1,5 @@
 export const data = {
     foundry: 'https://demo.foundryvtt.com/join',
-    'theatre-schedule': 'https://github.com/TheIthorian/theatre-schedule',
+    'theatre-schedule': 'https://ross-raid-army-exists.trycloudflare.com',
     'energy-dashboard': 'https://github.com/TheIthorian/energy-usage-dashboard',
 };
